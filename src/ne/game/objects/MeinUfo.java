677134f75601ac81ec04da27a048674584fdda2a
ElijaNicklaser <elija.nicklaser@student.htldornbirn.at>
2024-02-21 11:28:25 +0100

    First Commit

--- src/ne/game/objects/MeinUfo.java
+++ src/ne/game/objects/MeinUfo.java
@@ -4,9 +4,14 @@ import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Shape;
 
+import java.util.Random;
+
 public class MeinUfo extends SpielObjekt{
+    private float acceleration =0.01f;
+    private float geschwindigkeit =2;
     public MeinUfo(int x, int y, Image image) {
         super(x, y, image);
+        setRandomPosition();
     }
 
     @Override
@@ -21,10 +26,26 @@ public class MeinUfo extends SpielObjekt{
 
     @Override
     public void update(int delta) {
+        this.geschwindigkeit = (delta*this.acceleration + geschwindigkeit);
         if(this.getY() > (768+this.getHeight())){
-            this.setY(-this.getHeight());
+        this.setRandomPosition();
         }
-        this.setY(this.getY()+4);
-
+        this.setY(this.getY()+(int)this.geschwindigkeit);
     }
+    private void setRandomPosition(){
+        Random r = new Random();
+        int ry =0;
+        int rx =0;
+        rx = r.nextInt(1024-this.getWith()+1-0)+this.getWith()/2;
+        // y=0 oben
+        ry = r.nextInt(600+1+this.getHeight())+this.getHeight();
+        this.setY(-ry);
+        this.setX(rx);
+        setRandomAcceleration();
+    }
+    private void setRandomAcceleration(){
+        Random r = new Random();
+        this.geschwindigkeit = r.nextInt(20-1)+1;
+    }
+
 }
